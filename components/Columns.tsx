15c29ba9f1a4d0e5156b9a6ba1bca8f7b141504f
Steve Daniel <stevedaniel2003@gmail.com>
2023-07-13 12:05:54 +0530

    Headless UI imlementation of a modal component

--- components/Columns.tsx
+++ components/Columns.tsx
@@ -2,6 +2,7 @@ import { PlusCircleIcon } from "@heroicons/react/24/solid";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import TodoCard from "./TodoCard";
 import { useBoardStore } from "@/store/BoardStore";
+import { useModalStore } from "@/store/ModalStore";
 
 type Props = {
      id: TypedColumn,
@@ -20,6 +21,7 @@ const idToColumnText : {
 
 function Column({id, todos,index}: Props) {
      const [searchString] = useBoardStore((state)=> [state.searchString])
+     const openModal = useModalStore((state) => state.openModal);
 return ( 
 <Draggable draggableId={id} index={index}>
      {(provided) => (
@@ -66,8 +68,8 @@ return (
                        )})}
                        {provided.placeholder}
                        <div className="flex items-end justify-end p-2 ">
-                         <button className="text-green-500 hover:text-green-600" >
-                              <PlusCircleIcon className="h-10 w-10"/>
+                         <button onClick={openModal} className="text-green-500 hover:text-green-600" >
+          <PlusCircleIcon className="h-10 w-10"/>
                          </button>
                        </div>
                  </div>
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,22 +1,34 @@
 import { create } from 'zustand'
 import { getTodosGroupedByColumn } from '@/lib/getTodosGroupedByColumn'
-import { databases } from '@/appwrite';
+import { databases, storage } from '@/appwrite';
+
+
+
 interface BoardState {
+
 
     board: Board;
     getBoard: () => void;
     setBoardState: (board: Board) => void;
     updateTodoInDB: (todo: Todo, columnId : TypedColumn ) => void;
+   //  deleteTask : (taskIndex : number, todoId : Todo, id : TypedColumn) => void;
     searchString : string,
-    setSearchString : (searchString : string) => void
+    newTaskInput : string,
+    newTaskType : TypedColumn,
+    setNewTaskInput : (input : string) => void,
+    setNewTaskType : (type : TypedColumn) => void,
+    setSearchString : (searchString : string) => void,
+    deleteTask : (taskIndex : number, todo : Todo, id : TypedColumn) => void;
 }
 
 
-export const useBoardStore = create<BoardState>((set) => ({
+export const useBoardStore = create<BoardState>((set, get) => ({
   board: {
     columns: new Map<TypedColumn, Column>()
   },
   searchString: "",
+  newTaskInput : "",
+   newTaskType : "todo",
   setSearchString : (searchString) => set({searchString}),
   
   getBoard: async() => {
@@ -24,6 +36,22 @@ export const useBoardStore = create<BoardState>((set) => ({
      set({board})
   },
   setBoardState: (board) => set({board}),
+
+  setNewTaskInput : (input : string) => set({newTaskInput : input }),
+   setNewTaskType : (columnId : TypedColumn) => set({newTaskType : columnId }),
+
+   deleteTask : async (taskIndex, todo, id) => {
+        const newColumns = new Map(get().board.columns)
+        newColumns.get(id)?.todos.splice(taskIndex, 1)
+
+        set({ board : {columns : newColumns}})
+
+      //   if(todo.image){
+      //     await storage.deleteFile(todo.image.bucketId, todo.image.fileId);
+      //   }
+
+        await databases.deleteDocument(process.env.NEXT_PUBLIC_DATABASE_ID!,process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,todo.$id)
+   },
 
   updateTodoInDB: async (todo, columnId) => {
      await databases.updateDocument(process.env.NEXT_PUBLIC_DATABASE_ID!,process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,todo.$id, {
